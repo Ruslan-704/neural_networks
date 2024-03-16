15c388412e323a2d8985c93717c7bb56dbcdb2f6
Ruslan <ext_rbakhtiyarov@rencredit.ru>
2024-03-16 13:54:16 +0300

    refactoring lab3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,46 +1,49 @@
+# Импорт библиотек
 import pandas as pd
 import numpy as np
 import torch
 import torch.nn as nn
 
 
+# Модифицированный класс MLPtorch с тремя скрытыми слоями
 class MLPptorch(nn.Module):
-    def __init__(self,
-                 in_size,
-                 first_hidden_size,
-                 second_hidden_size,
-                 third_hidden_size,
-                 out_size):
-        nn.Module.__init__(self)
-        self.layers = nn.Sequential(nn.Linear(in_size, first_hidden_size),
-                                    nn.ReLU(),
-                                    nn.Linear(first_hidden_size, second_hidden_size),
-                                    nn.ReLU(),
-                                    nn.Linear(second_hidden_size, third_hidden_size),
-                                    nn.ReLU(),
-                                    nn.Linear(third_hidden_size, out_size),
-                                    nn.Sigmoid())
+    def __init__(self, in_size, first_hidden_size, second_hidden_size, third_hidden_size, out_size):
+        super(MLPptorch, self).__init__()
+        # Определение слоев нейронной сети
+        self.layers = nn.Sequential(
+            nn.Linear(in_size, first_hidden_size), nn.ReLU(),
+            nn.Linear(first_hidden_size, second_hidden_size), nn.ReLU(),
+            nn.Linear(second_hidden_size, third_hidden_size), nn.ReLU(),
+            nn.Linear(third_hidden_size, out_size), nn.ReLU()
+        )
 
-    # прямой проход
+    # Прямой проход
     def forward(self, x):
         return self.layers(x)
 
 
-# функция обучения
-def train(x, y, num_iter):
-    for i in range(0, num_iter):
-        pred = net.forward(x)
-        loss = lossFn(pred, y)
+# Функция обучения модели
+def train_model(x, y, num_iterations, net, loss_fn, optimizer):
+    for i in range(num_iterations):
+        # Прямой проход
+        pred = net(x)
+        # Рассчет функции потерь
+        loss = loss_fn(pred, y)
+        # Обратное распространение ошибки и обновление весов
         loss.backward()
         optimizer.step()
-        if i % 500 == 0:
-            print('Ошибка на ' + str(i) + ' итерации: ', loss.item())
+        # Вывод ошибки на экран каждые PRINT_INTERVAL итераций
+        if i % PRINT_INTERVAL == 0:
+            print(f'Ошибка на {i} итерации: {loss.item()}')
     return loss.item()
 
 
+# Загрузка данных
 df = pd.read_csv('data.csv')
+# Перемешивание данных
 df = df.iloc[np.random.permutation(len(df))]
 
+# Подготовка данных для обучения
 X = df.iloc[0:100, 0:3].values
 y = df.iloc[0:100, 4]
 y = y.map({'Iris-setosa': 1, 'Iris-virginica': 2, 'Iris-versicolor': 3}).values.reshape(-1, 1)
@@ -48,6 +51,7 @@ Y = np.zeros((y.shape[0], np.unique(y).shape[0]))
 for i in np.unique(y):
     Y[:, i - 1] = np.where(y == i, 1, 0).reshape(1, -1)
 
+# Подготовка данных для тестирования
 X_test = df.iloc[100:150, 0:3].values
 y = df.iloc[100:150, 4]
 y = y.map({'Iris-setosa': 1, 'Iris-virginica': 2, 'Iris-versicolor': 3}).values.reshape(-1, 1)
@@ -55,32 +59,37 @@ Y_test = np.zeros((y.shape[0], np.unique(y).shape[0]))
 for i in np.unique(y):
     Y_test[:, i - 1] = np.where(y == i, 1, 0).reshape(1, -1)
 
-inputSize = X.shape[1]  # количество входных сигналов равно количеству признаков задачи
-first_hidden_size = 50  # задаем число нейронов скрытого слоя
-second_hidden_size = 20  # задаем число нейронов скрытого слоя
-third_hidden_size = 10  # задаем число нейронов скрытого слоя
-outputSize = Y.shape[1] if len(Y.shape) else 1  # количество выходных сигналов равно количеству классов задачи
+# Определение размеров входа и выхода
+input_size = X.shape[1]
+first_hidden_size = 50
+second_hidden_size = 30
+third_hidden_size = 20
+output_size = Y.shape[1] if len(Y.shape) > 1 else 1
 
-net = MLPptorch(inputSize,
-                first_hidden_size,
-                second_hidden_size,
-                third_hidden_size,
-                outputSize)
-lossFn = nn.MSELoss()
-
-# создает оптимизатор Stochastic Gradient Descent (SGD) для обучения сети net
-# net.parameters() - веса
+# Создание модели
+net = MLPptorch(input_size, first_hidden_size, second_hidden_size, third_hidden_size, output_size)
+# Выбор функции потерь (Mean Squared Error)
+loss_fn = nn.MSELoss()
+# Выбор оптимизатора (Stochastic Gradient Descent)
 optimizer = torch.optim.SGD(net.parameters(), lr=0.009)
 
-#  запуск процесса обучения сети net с использованием функции train.
-loss_ = train(torch.from_numpy(X.astype(np.float32)),
-              torch.from_numpy(Y.astype(np.float32)), 5000)
+# Константы
+PRINT_INTERVAL = 200
+LEARNING_RATE = 0.009
+NUM_ITERATIONS = 5000
 
+# Обучение модели
+train_model(torch.Tensor(X.astype(np.float32)), torch.Tensor(Y.astype(np.float32)), NUM_ITERATIONS, net, loss_fn,
+            optimizer)
 
-pred = net.forward(torch.from_numpy(X.astype(np.float32))).detach().numpy()
-err = sum(abs((pred > 0.5) - Y))
-print(err)
+# Предсказание на тренировочных данных
+pred_train = net(torch.Tensor(X.astype(np.float32))).detach().numpy()
+# Вычисление ошибки на тренировочных данных
+err_train = sum(abs((pred_train > 0.5) - Y))
+print(err_train)
 
-pred = net.forward(torch.from_numpy(X_test.astype(np.float32))).detach().numpy()
-err = sum(abs((pred > 0.5) - Y_test))
-print(err)
+# Предсказание на тестовых данных
+pred_test = net(torch.Tensor(X_test.astype(np.float32))).detach().numpy()
+# Вычисление ошибки на тестовых данных
+err_test = sum(abs((pred_test > 0.5) - Y_test))
+print(err_test)
